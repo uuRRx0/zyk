7792b5b716c9a25b093ac069cec93c877ca027ab
yu00 <zhongyuhong@zyoker.com>
2021-09-09 10:30:54 +0800

    save bdv_id

--- common.js
+++ common.js
@@ -1,1 +1,17 @@
-console.log('hello zyk')
+function getBdvId(){
+  let bdv_id = window.location.search.split('&').find(item => item.includes('bdv_id=')).split('bdv_id=').pop()
+  const result = /\d+/.test(bdv_id) ? saveBdvId(bdv_id) : hasBdvId()
+  console.log('zyk', result)
+  return result
+}
+
+function saveBdvId(bdv_id){
+  window.localStorage.setItem('bdv_id', bdv_id)
+  return bdv_id
+}
+
+function hasBdvId(){
+  return window.localStorage.getItem('bdv_id')
+}
+
+document.onload = getBdvId
