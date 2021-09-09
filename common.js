9b4644402aa0c028cdd4782f8bd4f80985aa5242
yu00 <zhongyuhong@zyoker.com>
2021-09-09 10:54:07 +0800

    add remove function

--- common.js
+++ common.js
@@ -1,12 +1,13 @@
-function getBdvId(){
+function getBdvId(remove){
   let bdv_id = window.location.search.split('&').find(item => item.includes('bdv_id=')).split('bdv_id=').pop()
   const result = /\d+/.test(bdv_id) ? saveBdvId(bdv_id) : hasBdvId()
   console.log('zyk', result)
+  remove === true && removeBdvId()
   return result
 }
 
 function saveBdvId(bdv_id){
-  window.localStorage.setItem('bdv_id', bdv_id)
+  bdv_id && window.localStorage.setItem('bdv_id', bdv_id)
   return bdv_id
 }
 
@@ -14,4 +15,8 @@ function hasBdvId(){
   return window.localStorage.getItem('bdv_id')
 }
 
-document.onload = getBdvId
+function removeBdvId(){
+  window.localStorage.removeItem('bdv_id')
+}
+
+$(getBdvId)
